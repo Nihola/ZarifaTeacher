e310c0c2b2ca3bb4c4ca896d9d0c4daf7de3adec
Absalamova <zilolaxonabsalomova@gmail.com>
2025-08-04 22:50:55 +0500

    edit course page

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-import { courses } from "./CourseData";
+import { useTranslation } from "react-i18next";
+import { FaRegClock } from "react-icons/fa6";
 
 function Courses() {
+  const { t } = useTranslation();
+  const courseList = t("courseData", { returnObjects: true });
+
   return (
     <div className="min-h-screen bg-gray-100 p-6 py-30">
-      <h1 className="text-3xl font-bold text-center mb-10 text-blue-800">
-        Kurslar Ro‘yxati
-      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
-        {courses.map((course) => (
-          <div key={course.id} className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition">
-           
-            <h2 className="text-xl font-semibold text-blue-700 mb-2">{course.title}</h2>
-            <p className="text-gray-700 mb-4">{course.description}</p>
-            <p className="text-sm text-gray-600">Davomiyligi: {course.duration}</p>
-            <p className="text-lg font-bold mt-2">{course.price.toLocaleString()} so‘m / oy</p>
-            <button className="mt-4 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition">
-              Batafsil
+        {courseList.map((courseData, index) => (
+          <div key={index} className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition">
+            <h2 className="text-xl font-semibold text-blue-700 mb-2">
+              {courseData.title}
+            </h2>
+            <p className="text-lg font-bold ">
+              {courseData.newprice} 
+            </p>
+            <del className="text-lg font-bold mt-2 text-gray-500">
+              {courseData.price} 
+            </del>
+            <br />
+            <button className="mt-4 text-center bg-blue-600 text-white w-[99%] py-2 px-4 rounded hover:bg-blue-700 transition">
+              {t("courses.detailsBtn")}
             </button>
           </div>
         ))}
