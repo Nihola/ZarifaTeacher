7430968522b37586ee490941bb5d961c2f6954ef
Nihola <niholaadbugopirova@gmail.com>
2025-10-25 10:22:06 +0500

    trinhh

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Timer from './components/Timer';
 import { HelmetProvider } from "react-head";
+import SeoHeader from './components/SEOheader';
 export default function App() {
   useEffect(() => {
     AOS.init({ duration: 1000 });
@@ -14,6 +15,11 @@ export default function App() {
   return (
     <HelmetProvider>
       <Header/>
+      <SeoHeader
+                    title="ZarifaTeacher - O‘quv Markazi Toshkentda"
+                    description="IELTS va ingliz tili,IT,arab tili, rus tili Toshkentda. ZarifaTeacher — tajribali ustozlar bilan yuqori natijalarga erishing!"
+                    keywords="o‘quv markaz, IELTS kursi, ingliz tili, ZarifaTeacher, Toshkent, arab tili CEFR, rus tili , CEFR,IT kursi, Frontend,Python kursi"
+                  />
       <Timer/>
       <main className='pt-15 md:pt-20 sm:pt-60 max-sm:84'>
         <Outlet/>
