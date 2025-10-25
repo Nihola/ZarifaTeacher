bb4341e87368c76be4d930c2b5967f7ae024f0dc
Nihola <niholaadbugopirova@gmail.com>
2025-10-25 10:13:23 +0500

    trying to fix problem

--- src/components/SEOheader.jsx
+++ src/components/SEOheader.jsx
@@ -1,32 +1,35 @@
-import { Helmet } from "react-helmet-async";
+import { HeadProvider, Title, Meta, Link } from "react-head";
 
 export default function SeoHeader({
-    title,
-    description,
-    keywords,
-    image = "/preview.png",
-    url = "https://zarifateacher.uz",
+  title,
+  description,
+  keywords,
+  image = "/preview.png",
+  url = "https://zarifateacher.uz",
 }) {
-    return (
-        <Helmet>
-            {/* Basic SEO */}
-            <title>{title}</title>
-            <meta name="description" content={description} />
-            <meta name="keywords" content={keywords} />
-            <meta name="robots" content="index, follow" />
+  return (
+    <HeadProvider>
+      {/* Basic SEO */}
+      <Title>{title}</Title>
+      <Meta name="description" content={description} />
+      <Meta name="keywords" content={keywords} />
+      <Meta name="robots" content="index, follow" />
 
-            {/* Open Graph (for Facebook, Telegram, etc.) */}
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} />
-            <meta property="og:url" content={url} />
+      {/* Canonical link (helps avoid duplicate content issues) */}
+      <Link rel="canonical" href={url} />
 
-            {/* Twitter Card */}
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={image} />
-        </Helmet>
-    );
+      {/* Open Graph (Facebook, Telegram, etc.) */}
+      <Meta property="og:type" content="website" />
+      <Meta property="og:title" content={title} />
+      <Meta property="og:description" content={description} />
+      <Meta property="og:image" content={image} />
+      <Meta property="og:url" content={url} />
+
+      {/* Twitter Card */}
+      <Meta name="twitter:card" content="summary_large_image" />
+      <Meta name="twitter:title" content={title} />
+      <Meta name="twitter:description" content={description} />
+      <Meta name="twitter:image" content={image} />
+    </HeadProvider>
+  );
 }
